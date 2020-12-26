b84605adb2e1da7a9cd772b8d6ce84486355475f
Paul Cacheux <paulcacheux@gmail.com>
2020-12-26 22:05:13 +0100

    Change log interface

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::{rc::Rc, sync::RwLock};
 
 use ppu::PPU;
-use simple_logger::SimpleLogger;
 
 mod cpu;
 mod memory;
@@ -12,7 +11,7 @@ use cpu::CPU;
 use memory::Memory;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    SimpleLogger::new().init()?;
+    env_logger::init();
 
     let rom_path = std::env::args().nth(1).expect("Failed to get rom path");
     let rom_content = std::fs::read(&rom_path)?;
