63ff9a66f0ba4893a545b1c08e3be666ac866d6b
Paul Cacheux <paulcacheux@gmail.com>
2020-12-27 18:58:36 +0100

    Improve PPU implementation (Pixel FIFO and fetcher)

---
+++ src/ppu/fetcher.rs
@@ -0,0 +1,82 @@
+use crate::memory::Memory;
+
+#[derive(Debug, Clone)]
+pub struct Fetcher {
+    map_addr: u16,
+    addressing_mode: AddressingMode,
+    tile_x: u8,
+    tile_y: u8,
+}
+
+impl Fetcher {
+    pub fn new(
+        map_addr: u16,
+        addressing_mode: AddressingMode,
+        scroll_x: u8,
+        scroll_y: u8,
+        scan_line: u8,
+    ) -> Self {
+        let tile_x = scroll_x / 8;
+        let tile_y = ((scan_line + scroll_y) & 0xFF) / 8;
+
+        Fetcher {
+            map_addr,
+            addressing_mode,
+            tile_x,
+            tile_y,
+        }
+    }
+
+    pub fn fetch_pixels<M: Memory>(&mut self, memory: &mut M) -> [Pixel; 8] {
+        let offset = self.tile_y * 32 + self.tile_x;
+        let tile_id = memory.read_memory(self.map_addr) + offset;
+
+        let addr = match self.addressing_mode {
+            AddressingMode::From8000 => 0x8000 + (tile_id as u16) * 16,
+            AddressingMode::From8800 => {
+                let tile_id = tile_id as i8;
+                if tile_id >= 0 {
+                    0x9000 + (tile_id as u16) * 16
+                } else {
+                    0x9000 - (-tile_id as u16) * 16
+                }
+            }
+        };
+
+        let byte1 = memory.read_memory(addr);
+        let byte2 = memory.read_memory(addr + 1);
+
+        let mut pixels = [Pixel {
+            color: 0,
+            source: PixelSource::BackgroundWindow,
+        }; 8];
+
+        for (index, bit) in (0..8).rev().enumerate() {
+            let bit_low_value = (byte1 >> bit) & 0x1;
+            let bit_high_value = (byte2 >> bit) & 0x1;
+
+            let color_value = (bit_high_value << 1) | bit_low_value;
+            pixels[index].color = color_value;
+        }
+
+        self.tile_x = (self.tile_x + 1) % 32;
+        pixels
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Pixel {
+    pub color: u8,
+    pub source: PixelSource,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum PixelSource {
+    BackgroundWindow,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum AddressingMode {
+    From8000,
+    From8800,
+}
--- src/ppu/mod.rs
+++ src/ppu/mod.rs
@@ -1,8 +1,10 @@
-use std::thread::current;
+use std::collections::VecDeque;
 
 use crate::memory::Memory;
 use bitflags::bitflags;
-use log::debug;
+
+mod fetcher;
+use fetcher::*;
 
 bitflags! {
     pub struct ControlReg: u8 {
@@ -13,7 +15,7 @@ bitflags! {
         const BG_TILE_MAP_DISPLAY_SELECT = 1 << 3;
         const OBJ_SIZE = 1 << 2;
         const OBJ_DISPLAY_ENABLE = 1 << 1;
-        const BG_WINDOW_DISPLAY_PRIORITY = 1 << 1;
+        const BG_WINDOW_DISPLAY_PRIORITY = 1 << 0;
     }
 }
 
@@ -21,23 +23,35 @@ bitflags! {
 pub struct PPU<M: Memory> {
     memory: M,
     current_dot_in_line: u32,
+    frame: [u8; PIXEL_COUNT],
+    fetcher: FetcherState,
+    fifo: VecDeque<Pixel>,
 }
 
+const SCREEN_WIDTH: u8 = 160;
 const SCREEN_HEIGHT: u8 = 144;
+const PIXEL_COUNT: usize = (SCREEN_WIDTH as usize) * (SCREEN_HEIGHT as usize);
+
 const SCAN_LINE_COUNT: u8 = SCREEN_HEIGHT + 10;
 const DOT_PER_LINE_COUNT: u32 = 80 + 172 + 204;
 const HBLANK_START: u32 = 172 + 80;
 
 const LCD_CONTROL_REG_ADDR: u16 = 0xFF40;
 const LCD_STATUS_REG_ADDR: u16 = 0xFF41;
+const LCD_SCROLL_Y_ADDR: u16 = 0xFF42;
+const LCD_SCROLL_X_ADDR: u16 = 0xFF43;
 const LCD_LY_ADDR: u16 = 0xFF44;
 const LCD_LYC_ADDR: u16 = 0xFF45;
+const BG_PALETTE_DATA_ADDR: u16 = 0xFF47;
 
 impl<M: Memory> PPU<M> {
     pub fn new(memory: M) -> Self {
         PPU {
             memory,
             current_dot_in_line: 0,
+            frame: [0; PIXEL_COUNT],
+            fetcher: FetcherState::Waiting,
+            fifo: VecDeque::new(),
         }
     }
 
@@ -48,6 +62,28 @@ impl<M: Memory> PPU<M> {
 
     fn current_line(&self) -> u8 {
         self.memory.read_memory(LCD_LY_ADDR)
+    }
+
+    fn current_mode(&self) -> Mode {
+        let current_line = self.current_line();
+        if current_line >= SCREEN_HEIGHT {
+            Mode::VBlank
+        } else if self.current_dot_in_line < 80 {
+            Mode::OAMSearch
+        } else if self.current_dot_in_line < HBLANK_START {
+            Mode::LCDTransfer
+        } else {
+            Mode::HBlank
+        }
+    }
+
+    fn update_status_reg(&mut self) {
+        let coincidence = self.current_line() == self.memory.read_memory(LCD_LYC_ADDR);
+
+        let updated_part = ((coincidence as u8) << 3) | (self.current_mode() as u8);
+        let old_reg = self.memory.read_memory(LCD_STATUS_REG_ADDR);
+        self.memory
+            .write_memory(LCD_STATUS_REG_ADDR, (old_reg & 0xF0) | updated_part);
     }
 
     fn next_dot(&mut self) {
@@ -62,35 +98,65 @@ impl<M: Memory> PPU<M> {
             }
             self.memory.write_memory(LCD_LY_ADDR, current_line);
         }
+    }
 
-        // update status reg
-        let coincidence = self.current_line() == self.memory.read_memory(LCD_LYC_ADDR);
+    fn prepare_frame_and_fetcher(&mut self) {
+        for pixel in &mut self.frame {
+            *pixel = 0;
+        }
 
-        let mode = if current_line >= SCREEN_HEIGHT {
-            1 // V-blank
-        } else if self.current_dot_in_line < 80 {
-            2 // Search OAM
-        } else if self.current_dot_in_line < HBLANK_START {
-            3 // Transfer data to LCD
-        } else {
-            0 // H-blank
-        };
+        self.fifo.clear();
+        self.next_fetcher_state();
+    }
 
-        let updated_part = ((coincidence as u8) << 3) | mode;
-        let old_reg = self.memory.read_memory(LCD_STATUS_REG_ADDR);
-        self.memory
-            .write_memory(LCD_STATUS_REG_ADDR, (old_reg & 0xF0) | updated_part);
+    fn next_fetcher_state(&mut self) {
+        let lcdc = self.control_reg();
+        let scan_line = self.current_line();
+        self.fetcher.next_update(lcdc, &mut self.memory, scan_line);
     }
 
     fn cycle(&mut self) {
-        if self.current_line() >= SCREEN_HEIGHT {
-            debug!("V-Blank");
-        } else if self.current_dot_in_line == 0 {
-            debug!("Start OAM search");
-        } else if self.current_dot_in_line == 80 {
-            debug!("Start updating frame");
-        } else if self.current_dot_in_line == HBLANK_START {
-            debug!("Start H-Blank")
+        self.update_status_reg();
+
+        let current_mode = self.current_mode();
+        match current_mode {
+            Mode::HBlank => {}
+            Mode::VBlank => {}
+            Mode::OAMSearch => {}
+            Mode::LCDTransfer => {
+                if self.current_dot_in_line == 80 {
+                    self.prepare_frame_and_fetcher();
+                } else if self.current_dot_in_line > 80 {
+                    self.next_fetcher_state();
+                    let scan_line = self.current_line();
+
+                    match &mut self.fetcher {
+                        FetcherState::Waiting => {}
+                        FetcherState::Ready(fetcher) => {
+                            self.fifo.extend(&fetcher.fetch_pixels(&mut self.memory));
+
+                            let scroll_x = self.memory.read_memory(LCD_SCROLL_X_ADDR);
+                            for _ in 0..(scroll_x % 8) {
+                                self.fifo.pop_front();
+                            }
+                        }
+                        FetcherState::Working(fetcher, x) => {
+                            if self.fifo.len() < 8 {
+                                self.fifo.extend(&fetcher.fetch_pixels(&mut self.memory));
+                            }
+
+                            let pixel = self.fifo.pop_front().unwrap();
+                            let offset =
+                                (scan_line as usize) * (SCREEN_WIDTH as usize) + (*x as usize);
+
+                            let bg_palette = self.memory.read_memory(BG_PALETTE_DATA_ADDR);
+                            let actual_color = bg_palette >> (pixel.color * 2) & 0b11;
+
+                            self.frame[offset] = actual_color;
+                        }
+                    }
+                }
+            }
         }
 
         self.next_dot();
@@ -100,5 +166,65 @@ impl<M: Memory> PPU<M> {
         for _ in 0..4 {
             self.cycle();
         }
+    }
+}
+
+#[repr(u8)]
+enum Mode {
+    HBlank = 0,
+    VBlank = 1,
+    OAMSearch = 2,
+    LCDTransfer = 3,
+}
+
+#[derive(Debug, Clone)]
+enum FetcherState {
+    Waiting,
+    Ready(Fetcher),
+    Working(Fetcher, u8),
+}
+
+impl Default for FetcherState {
+    fn default() -> Self {
+        FetcherState::Waiting
+    }
+}
+
+impl FetcherState {
+    fn next(self, lcdc: ControlReg, memory: &mut dyn Memory, scan_line: u8) -> Self {
+        match self {
+            FetcherState::Waiting => {
+                let fetcher = Fetcher::new(
+                    if lcdc.contains(ControlReg::BG_TILE_MAP_DISPLAY_SELECT) {
+                        0x9C00
+                    } else {
+                        0x9800
+                    },
+                    if lcdc.contains(ControlReg::BG_WINDOW_TILE_DATA_SELECT) {
+                        AddressingMode::From8000
+                    } else {
+                        AddressingMode::From8800
+                    },
+                    memory.read_memory(LCD_SCROLL_X_ADDR),
+                    memory.read_memory(LCD_SCROLL_Y_ADDR),
+                    scan_line,
+                );
+                FetcherState::Ready(fetcher)
+            }
+            FetcherState::Ready(fetcher) => FetcherState::Working(fetcher, 0),
+            FetcherState::Working(fetcher, x) => {
+                if x + 1 == SCREEN_WIDTH {
+                    FetcherState::Waiting
+                } else {
+                    FetcherState::Working(fetcher, x + 1)
+                }
+            }
+        }
+    }
+
+    fn next_update(&mut self, lcdc: ControlReg, memory: &mut dyn Memory, scan_line: u8) {
+        let current = std::mem::take(self);
+        let next = current.next(lcdc, memory, scan_line);
+        let _ = std::mem::replace(self, next);
     }
 }
