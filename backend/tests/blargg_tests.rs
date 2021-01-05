c9313cc6e99b09eda158b2bc20459fff2112a376
Paul Cacheux <paulcacheux@gmail.com>
2021-01-05 11:15:45 +0100

    Better blargg tests

--- backend/tests/blargg_tests.rs
+++ backend/tests/blargg_tests.rs
@@ -22,23 +22,40 @@ impl SerialWrite for SerialDebug {
     }
 }
 
-const EXPECTED_OUTPUT: &str = "cpu_instrs\n\n01:ok  02:ok  03:ok  04:ok  05:ok  06:ok  07:ok  08:ok  09:ok  10:ok  11:ok  \n\nPassed all tests";
+const CPU_INSTRS_EXPECTED_OUTPUT: &str = "cpu_instrs\n\n01:ok  02:ok  03:ok  04:ok  05:ok  06:ok  07:ok  08:ok  09:ok  10:ok  11:ok  \n\nPassed all tests";
+const INSTR_TIMING_EXPECTED_OUTPUT: &str = "instr_timing\n\n\nPassed";
 
-#[test]
-fn test_blargg_cpu_instrs() {
-    let rom_path = "./test_roms/blargg/cpu_instrs.gb";
+fn blargg_test(rom_path: &str, timemout: std::time::Duration, expected_output: &str) {
     let output_content = Arc::new(Mutex::new(String::new()));
     let serial: SerialPtr = Box::new(SerialDebug::new(output_content.clone()));
     let mut emu = common::setup_rom(rom_path, Some(serial));
 
     let start_time = std::time::Instant::now();
 
-    while start_time.elapsed() < std::time::Duration::from_secs(10 * 60)
-        && output_content.lock().unwrap().len() < EXPECTED_OUTPUT.len()
+    while start_time.elapsed() < timemout
+        && output_content.lock().unwrap().len() < expected_output.len()
     {
         emu.cpu.step();
         emu.ppu.step();
     }
 
-    assert_eq!(output_content.lock().unwrap().as_str(), EXPECTED_OUTPUT);
+    assert_eq!(output_content.lock().unwrap().as_str(), expected_output);
+}
+
+#[test]
+fn test_blargg_cpu_instrs() {
+    blargg_test(
+        "./test_roms/blargg/cpu_instrs.gb",
+        std::time::Duration::from_secs(10 * 60),
+        CPU_INSTRS_EXPECTED_OUTPUT,
+    );
+}
+
+#[test]
+fn test_blargg_instr_timing() {
+    blargg_test(
+        "./test_roms/blargg/instr_timing.gb",
+        std::time::Duration::from_secs(2 * 60),
+        INSTR_TIMING_EXPECTED_OUTPUT,
+    );
 }
